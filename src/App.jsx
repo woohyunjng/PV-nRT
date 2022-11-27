8b742694d7b5a8ebfa9b03cd534199366b3cc3a0
woohyunjng <woohyun.jng08@gmail.com>
2022-11-27 16:04:37 +0900

    feat(material-ui): use material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
     return (
-        <h1>Hello, World!</h1>
+        <Button variant="contained">Hello World</Button>
     )
 }
 
